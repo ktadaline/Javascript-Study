ab63a84c98be8341424fe913fed989c8994b660c
Katie Adaline Sroub <ksroub@gmail.com>
2016-11-08 11:47:24 -0500

    Basics

---
+++ assignmentoperators.js
@@ -0,0 +1,49 @@
+//assignment operators assign values to JavaScript variables
+
+/*
+Operator    Example   Is equivalent to
+
+=           x = y     x = y
++=          x += y    x = x + y
+-=          x -= y    x = x - y
+*=          x *= y    x = x * y
+/=          x /= y    x = x / y
+%=          x %= y    x = x  % y
+ */
+
+var bk =  "<br /> " ;
+document.write (bk);
+
+
+var star = 25;
+ star += 3;
+document.write(star);
+
+var bk =  "<br /> " ;
+document.write (bk);
+
+
+var moon = 100;
+moon -= 12;
+document.write(moon);
+
+var bk =  "<br /> " ;
+document.write (bk);
+
+
+moon *= star;
+document.write(moon);
+
+var bk =  "<br /> " ;
+document.write (bk);
+
+
+moon /= 10;
+document.write(moon);
+
+var bk =  "<br /> " ;
+document.write (bk);
+
+var mars = 14;
+mars %= 3;
+document.write(mars);
