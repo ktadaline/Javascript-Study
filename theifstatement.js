3fdd4ea2d28171fd37b798dc2f02e8b243231a2d
Katie Adaline Sroub <ksroub@gmail.com>
2016-11-08 15:43:52 -0500

    Conditionals and Loops

---
+++ theifstatement.js
@@ -0,0 +1,24 @@
+/*
+The if Statement
+
+Very often when you write code, you want to perform different actions based on different conditions.
+You can do this by using conditional statements in your code.
+
+Use if to specify a block of code that will be executed if a specified condition is true.
+
+if (condition){
+    statements
+}
+ */
+
+var myNum1 = 28;
+var myNum2 = 33;
+if (myNum1 < myNum2) {
+    alert("yay");
+}
+
+var a1 = 28;
+var a2 = 33;
+if (a1 < a2) {
+    alert("nay");
+}
