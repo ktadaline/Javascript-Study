cc85ada5ca7e86e72a4d1190012a1f04368a7600
Katie Adaline Sroub <ksroub@gmail.com>
2016-11-08 12:00:14 -0500

    Basics

---
+++ comparisonoperators.js
@@ -0,0 +1,24 @@
+/*Comparison Operators are used in logical statements to determine equality or difference between
+variables or values. They return true or false.
+
+The equal to (==) operator checks whether the operands' values are equal */
+
+var num = 28;
+document.write(num == 18);
+
+/*
+Operator    Description                  Example
+
+==          equal to                     5 == 10 false
+===         identical
+            (equal and of same type)     5 === 10 false
+!=          not equal to                 5 !=10 true
+>           greater than                 10 > 5 true
+>=          greater than or equal to     10 >= 5 true
+<           less than                    10 < 5 false
+<=          less than or equal to        10 <= 5 false
+
+
+when using operators, be sure that the arguments are of the same data type; numbers should be
+compared with numbers, strings with strings and so on..
+ */
