1da4d97455bc27fc680491d294501d91ecebc44d
Katie Adaline Sroub <ksroub@gmail.com>
2017-01-14 19:58:38 -0500

    DOM and Events

--- Creating Animations.js
+++ Creating Animations.js
@@ -1,5 +1,6 @@
 /*
 ANIMATIONS
 
-
+Now that we know how to select and change DOM elements, we can create a simple animation.
+lets create a simple HTML page with just a box element that will be animated using JS.
  */
