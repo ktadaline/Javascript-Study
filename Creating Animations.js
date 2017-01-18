c335a43943c2eb9ed7831c092d9ebef0c8716a63
Katie Adaline Sroub <ksroub@gmail.com>
2017-01-18 13:25:09 -0500

    DOM and Events

--- Creating Animations.js
+++ Creating Animations.js
@@ -1,5 +1,13 @@
 /*
-ANIMATIONS
+Animations
 
+Now that we know how to select and create DOM events, we can create a simple animation. Let's create a simple HTML page
+with a box element that will be animated using JS.
+
+To create an animation, we need to change the properties of an element at small intervals of time. We can achieve this
+by using the setInterval()method, which allows us to create a timer and call a function to change properties repeatedly
+at defined intervals (in milliseconds)
 
  */
+
+var t - setInter
